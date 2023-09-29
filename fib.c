d25740d0a20f4153f8158736d343f328ba7ad2ac
Liam Fernandez <lgf001@ucsd.edu>
2023-09-29 15:42:51 -0700

    Added starter code for "Basic C Implementation"

---
+++ fib.c
@@ -0,0 +1,20 @@
+/**
+ * This file contains the implementation of a function that evaluates and
+ * returns the nth term of the fibonaci sequence
+ */
+
+#include <stdio.h>  // allows us to use printf(...)
+
+/**
+ * Returns the nth term in the fibonacci sequence
+ * 
+ * @param n : term to return 
+ * @return int : nth term in the fibonacci sequence
+ */
+int get_nth_fib(int n) {
+
+}
+
+int main() {
+
+}
