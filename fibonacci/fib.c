01867c0ccbd41549dea26cc82dae13f41f478b60
Mihir Kekkar <mkekkar@ucsd.edu>
2023-10-02 11:30:35 -0700

    update comments

--- fibonacci/fib.c
+++ fibonacci/fib.c
@@ -1,6 +1,6 @@
 /**
  * Returns the nth term in the fibonacci sequence.
- * The sequence should be 0-indexed, i.e. F(0) = 0, F(1) = 1, F(2) = 2, ...
+ * The sequence should be 0-indexed, i.e. F(0) = 0, F(1) = 1, F(2) = 1, ...
  *
  * @param n : index of term to return
  * @return the nth term in the fibonacci sequence
--- fibonacci/fib.h
+++ fibonacci/fib.h
@@ -1,4 +1,6 @@
 /**
  * Header file containing declaration for get_nth_fib function
+ *
+ * No changes required
  */
 int get_nth_fib(int n);
